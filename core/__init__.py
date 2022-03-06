70fb85fdc9d031fa26a540fc5bdf0330619eb9c1
Mansur <mansur.ham@mail.ru>
2022-03-06 22:35:48 +0300

    final

--- core/__init__.py
+++ core/__init__.py
@@ -2,19 +2,9 @@ from flask.config import Config
 from flask import Flask
 
 from config import Config
-from flask_mail import Mail, Message
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
-mail= Mail(app)
-
-app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'mansur.ham44@gmail.com'
-app.config['MAIL_PASSWORD'] = 'Xamerzaev95'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app)
 
 from core  import routes
--- core/routes.py
+++ core/routes.py
@@ -1,16 +1,8 @@
 from flask import render_template, flash
 
 from core import app
-from flask import request
-from flask_mail import Message
-from . import mail
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        text = request.form.get('text')    
-        msg = Message("С вами хочет связаться новый клиент!",sender="mansur.ham44@gmail.com" ,recipients=['mansur.ham44@gmail.com'])
-        msg.body = f"Мансур, красавчик, у тебя новый заказ. cвяжись мой босс с: {text}"
-        mail.send(msg)
     return render_template('index.html')
